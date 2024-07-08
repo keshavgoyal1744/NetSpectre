981c1918c0c136c29cc9fe20a85551bb5c9d9b91
Keshav Goyal <keshavgoyal@willow.rlogin>
2024-07-08 00:38:48 -0400

    Initial commit

--- frontend/src/components/Timeline.js
+++ frontend/src/components/Timeline.js
@@ -1,6 +1,6 @@
 /*
  * This file is part of caronte (https://github.com/eciavatta/caronte).
- * Copyright (c) 2020 Emiliano Ciavatta.
+ * Copyright (c) 2020 Keshav Goyal.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
